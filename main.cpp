8058a2b5e65377f3e7d53ba2016bf46e1edc132e
Arkadiusz Lach <lach@includespark.eu>
2019-08-23 23:45:35 +0200

    main.cpp cleanup

--- main.cpp
+++ main.cpp
@@ -6,20 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    /*QMessageBox::StandardButton reply;
-
-    reply = QMessageBox::question(0, "Save", "Do you want to save your changes?",
-        QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);*/
-
     MainWindow w;
-
-    //QSurfaceFormat format;
-    //format.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
-    //format.setSwapInterval(0);
-    //QSurfaceFormat::setDefaultFormat(format);
-
-    //w.showMaximized();
     w.show();
 
     return a.exec();
